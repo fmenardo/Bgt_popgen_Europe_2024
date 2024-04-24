c10d975987520015e1a70f6ee83dd749fbdfe8fb
Jigisha <90404355+jjigisha@users.noreply.github.com>
2024-04-24 17:06:32 +0200

    updated feems results
    using updated dataset

---
+++ FEEMS/run_feems.py
@@ -0,0 +1,205 @@
+import argparse
+import re
+import os
+
+import numpy as np
+#import pkg_resources
+from sklearn.impute import SimpleImputer
+from pandas_plink import read_plink
+
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import cartopy.crs as ccrs
+
+from feems.utils import prepare_graph_inputs
+from feems import SpatialGraph, Viz
+from feems.cross_validation import run_cv
+
+
+# change matplotlib fonts
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams["font.sans-serif"] = "Arial"
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i','--input_plink_prefix',type=str,help='prefix of plink files to be used')
+parser.add_argument('-sc','--sample_coordinates',type=str,help='file with sample coordinates in long lat format')
+parser.add_argument('-oc','--outer_coordinates',type=str,help='file with outer polygon coordinates in long lat format')
+parser.add_argument('-dgg','--discrete_global_grid',type=str,help='discrete global grid file')
+
+ag = parser.parse_args()
+
+#### data ####
+
+# read the genotype data and mean impute missing data
+(bim, fam, G) = read_plink(ag.input_plink_prefix)
+imp = SimpleImputer(missing_values=np.nan, strategy="mean")
+genotypes = imp.fit_transform((np.array(G)).T)
+
+print (f"n_samples={genotypes.shape[0]}, n_snps={genotypes.shape[1]}")
+
+# setup graph
+
+coord = np.loadtxt(ag.sample_coordinates) 
+outer = np.loadtxt(ag.outer_coordinates)
+grid_path = str(ag.discrete_global_grid)
+
+# graph input files
+outer, edges, grid, _ = prepare_graph_inputs(coord=coord, 
+                                             ggrid=grid_path,
+                                             translated=False, 
+                                             buffer=0,
+                                             outer=outer)
+
+
+print(f"outer {outer[:5,]}")
+print(f"edges {edges[:5,]}")
+print(f"grid {grid[:5,]}")
+
+#### set up SpatialGraph obj ####
+
+sp_graph = SpatialGraph(genotypes, coord, grid, edges, scale_snps=True)
+
+projection = ccrs.EquidistantConic(central_longitude=13.913409166547657, central_latitude=48.610721843508884)
+
+
+# map of sample coordinates, graph and observed nodes
+
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=10, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_samples()
+v.draw_edges(use_weights=False)
+v.draw_obs_nodes(use_ids=False)
+plt.savefig(f'{ag.input_plink_prefix}_sample_grid_map_{ag.discrete_global_grid[:-4]}.pdf')
+
+
+
+#### run CV ####
+
+# define grids
+# reverse the order of lambdas and alphas for warmstart
+lamb_grid = np.geomspace(1e-6, 1e2, 20)[::-1]
+
+# run cross-validation
+cv_err = run_cv(sp_graph, lamb_grid, n_folds=sp_graph.n_observed_nodes, factr=1e10)
+
+# average over folds
+mean_cv_err = np.mean(cv_err, axis=0)
+
+# argmin of cv error
+lamb_cv = float(lamb_grid[np.argmin(mean_cv_err)])
+
+# plot CV error
+fig, ax = plt.subplots(dpi=300)
+ax.plot(np.log10(lamb_grid), mean_cv_err, ".");
+ax.set_xlabel("log10(lambda)");
+ax.set_ylabel("L2 CV Error");
+ax.axvline(np.log10(lamb_cv), color = "orange")
+plt.savefig(f'{ag.input_plink_prefix}_CV_error_{ag.discrete_global_grid[:-4]}.pdf')
+
+#### fit feems ####
+
+sp_graph.fit(lamb_cv)
+
+# plot
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_edges(use_weights=True)
+v.draw_obs_nodes(use_ids=False) 
+v.draw_edge_colorbar()
+plt.savefig(f'{ag.input_plink_prefix}_feems_plot_{ag.discrete_global_grid[:-4]}.pdf')
+
+
+
+#### plot for different lambda values ####
+
+# figure params
+
+title_loc = "left"
+title_pad = "-10"
+title_fontsize = 12
+edge_width = .2
+edge_alpha = 1
+edge_zorder = 3
+obs_node_size = 3
+obs_node_linewidth = .4
+cbar_font_size = 8
+cbar_ticklabelsize = 8
+cbar_orientation = "horizontal"
+
+# figure setup
+fig = plt.figure(dpi=300)
+spec = gridspec.GridSpec(ncols=2, nrows=2, figure=fig, wspace=0.0, hspace=0.0)
+
+# axis 00 
+ax_00 = fig.add_subplot(spec[0, 0], projection=projection)
+ax_00.set_title("A", loc=title_loc, pad=title_pad, fontdict={"fontsize": title_fontsize})
+sp_graph.fit(float(lamb_grid[0]))
+v = Viz(ax_00, sp_graph, projection=projection, edge_width=edge_width, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=obs_node_size, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_edges(use_weights=True)
+v.draw_obs_nodes(use_ids=False) 
+ax_00.text(.7, .85, "lambda={:.5f}\ncv l2 error={:.5f}".format(lamb_grid[0], mean_cv_err[0, 0]), 
+           fontdict={"fontsize": 4}, transform = ax_00.transAxes)
+
+# axis 10
+ax_10 = fig.add_subplot(spec[1, 0], projection=projection)
+ax_10.set_title("B", loc=title_loc, pad=title_pad, fontdict={"fontsize": title_fontsize})
+sp_graph.fit(float(lamb_grid[3]))
+v = Viz(ax_10, sp_graph, projection=projection, edge_width=edge_width, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20,
+        obs_node_size=obs_node_size, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_edges(use_weights=True)
+v.draw_obs_nodes(use_ids=False) 
+ax_10.text(.7, .85, "lambda={:.5f}\ncv l2 error={:.5f}".format(lamb_grid[3], mean_cv_err[3, 0]), 
+           fontdict={"fontsize": 4}, transform = ax_10.transAxes)
+
+# axis 01
+ax_01 = fig.add_subplot(spec[0, 1], projection=projection)
+ax_01.set_title("C", loc=title_loc, pad=title_pad, fontdict={"fontsize": title_fontsize})
+sp_graph.fit(float(lamb_cv))
+v = Viz(ax_01, sp_graph, projection=projection, edge_width=edge_width, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=obs_node_size, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_edges(use_weights=True)
+v.draw_obs_nodes(use_ids=False) 
+ax_01.text(.7, .85, "lambda={:.5f}\ncv l2 error={:.5f}".format(lamb_cv, mean_cv_err[np.argmin(mean_cv_err), 0]), 
+           fontdict={"fontsize": 4}, transform = ax_01.transAxes)
+
+# axis 11
+ax_11 = fig.add_subplot(spec[1, 1], projection=projection)
+ax_11.set_title("D", loc=title_loc, pad=title_pad, fontdict={"fontsize": title_fontsize})
+sp_graph.fit(float(lamb_grid[10]))
+v = Viz(ax_11, sp_graph, projection=projection, edge_width=edge_width, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=obs_node_size, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_edges(use_weights=True)
+v.draw_obs_nodes(use_ids=False)
+v.cbar_font_size = cbar_font_size
+v.cbar_orientation = cbar_orientation
+v.cbar_ticklabelsize = cbar_ticklabelsize
+v.draw_edge_colorbar()
+ax_11.text(.7, .85, "lambda={:.5f}\ncv l2 error={:.5f}".format(lamb_grid[10], mean_cv_err[10, 0]), 
+           fontdict={"fontsize": 4}, transform = ax_11.transAxes)
+
+plt.savefig(f'{ag.input_plink_prefix}_feems_plot_{ag.discrete_global_grid[:-4]}_lambda_compare.pdf')
