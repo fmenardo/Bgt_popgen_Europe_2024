cb2d8fad11756f3547bd80eb55476cf240ba7622
Jigisha <90404355+jjigisha@users.noreply.github.com>
2024-07-09 16:20:14 +0200

    Update plot_pca_clean.R

--- PCA/plot_pca_clean.R
+++ PCA/plot_pca_clean.R
@@ -15,39 +15,41 @@ world_eig$var_perc <- (world_eig$pca.eig/sum(world_eig$pca.eig))*100
 global_regions <- read.csv("../../data/global_regions.csv")
 world_pca_reg <- merge(world_pca, global_regions, by.x = "isolate", by.y = "name")
 
+world_pca_reg$collection_3_levels <- ifelse(world_pca_reg$Collection == 2022 | world_pca_reg$Collection == 2023, 
+                                            world_pca_reg$Collection,
+                                                    "1980-2019")
 
 # percentage of variance explained by each principal component
-ggplot(data=world_eig[1:20,], aes(PC, var_perc))+geom_bar(stat = "identity")+
+var <- ggplot(data=world_eig[1:20,], aes(PC, var_perc))+geom_bar(stat = "identity")+
   theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs(x="Principal Component", y="Percentage of variance explained",title = "PCA - World dataset")+
   scale_x_continuous(breaks = c(1:20))
 
-
 # PC1 vs PC2
-world_pc12 <- ggplot(world_pca_reg, aes(PC1, PC2, colour=global_region,shape=Collection)) + geom_point(size=3) + theme_classic(base_size = 14)+
+world_pc12 <- ggplot(world_pca_reg, aes(PC1, PC2, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC1", " ", "(", signif(world_eig$var_perc[1], 3),"%", ")",  sep = ""), 
         y = paste("PC2", " ", "(", signif(world_eig$var_perc[2], 3), "%",")",  sep = ""),
-        colour = "Region", shape = "Collection") + scale_color_manual(values = as.vector(kelly(12))[2:12]) 
+        colour = "Region", shape = "Year of sampling") + scale_color_manual(values = as.vector(kelly(12))[2:12]) 
 
 # PC1 vs PC3
-world_pc13 <- ggplot(world_pca_reg, aes(PC1, PC3, colour=global_region,shape=Collection)) + geom_point(size=3) + theme_classic(base_size = 14)+
+world_pc13 <- ggplot(world_pca_reg, aes(PC1, PC3, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC1", " ", "(", signif(world_eig$var_perc[1], 3),"%", ")",  sep = ""), 
         y = paste("PC3", " ", "(", signif(world_eig$var_perc[3], 3), "%",")",  sep = ""),
-        colour = "Region", shape = "Collection") + scale_color_manual(values = as.vector(kelly(12))[2:12]) 
+        colour = "Region", shape = "Year of sampling") + scale_color_manual(values = as.vector(kelly(12))[2:12]) 
 
 #PC2 vs PC3
-world_pc23 <- ggplot(world_pca_reg, aes(PC2, PC3, colour=global_region,shape=Collection)) + geom_point(size=3) + theme_classic(base_size = 14)+
+world_pc23 <- ggplot(world_pca_reg, aes(PC2, PC3, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC2", " ", "(", signif(world_eig$var_perc[2], 3),"%", ")",  sep = ""), 
         y = paste("PC3", " ", "(", signif(world_eig$var_perc[3], 3), "%",")",  sep = ""),
-        colour = "Region", shape = "Collection") + scale_color_manual(values = (kelly(12))[2:12]) 
+        colour = "Region", shape = "Year of sampling") + scale_color_manual(values = (kelly(12))[2:12]) 
 
-world_all <- world_pc12 + world_pc13 + world_pc23 + guide_area() + plot_layout(guides = "collect") & guides(shape=guide_legend(order = 1, ncol = 2 ), colour = guide_legend(order = 2))
+world_all <- world_pc12 + world_pc13 + world_pc23 + var + plot_layout(guides = "collect")+plot_annotation(tag_levels = 'a')
 
-ggsave(world_all, filename = "tritici_world_regions_pca_plots.pdf", height = 30, width = 45, unit = "cm")
+#ggsave(world_all, filename = "tritici_world_regions_pca_plots.pdf", height = 30, width = 45, unit = "cm")
 
 #### Europe+ dataset ####
 
@@ -56,39 +58,43 @@ eur_eig <- read.csv("tritici_europe+.eig.csv")
 eur_eig$PC <- as.numeric(rownames(eur_eig))
 eur_eig$var_perc <- (eur_eig$pca.eig/sum(eur_eig$pca.eig))*100
 
+eur_pca$collection_3_levels <- ifelse(eur_pca$Collection == 2022 | eur_pca$Collection == 2023, 
+                                            eur_pca$Collection,
+                                            "1980-2019")
+
 # percentage of variance explained by each principal component
-ggplot(data=eur_eig[1:20,], aes(PC, var_perc))+geom_bar(stat = "identity")+
+var_eur <- ggplot(data=eur_eig[1:20,], aes(PC, var_perc))+geom_bar(stat = "identity")+
   theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
-  labs(x="Principal Component", y="Percentage of variance explained",title = "PCA - Europe+ dataset")+
+  labs(x="Principal Component", y="Percentage of variance explained")+
   scale_x_continuous(breaks = c(1:20))
 
-# modified as.vector(polychrome(30)) to better distinguish between russia and turkey
+## modified as.vector(polychrome(30)) to better distinguish between russia and turkey
 my_pal <- c("#5A5156", "#E4E1E3", "#F6222E", "#FE00FA", "#16FF32", "#3283FE", "#FEAF16", "#B00068", "#1CFFCE", "#90AD1C", "#2ED9FF", "#DEA0FD", "#AA0DFE", "#F8A19F", "#325A9B",
             "#C4451C", "#B10DA1", "#85660D", "#1C8356", "#FBE426", "#1CBE4F", "#FA0087", "#FC1CBF", "#F7E1A0", "#C075A6", "#782AB6", "#AAF400", "#BDCDFF", "#822E1C", "#B5EFB5")
 
 # PC1 vs PC2
-eur_pc12 <- ggplot(eur_pca, aes(PC1, PC2, colour=Country,shape=Collection)) + geom_point(size=3) + theme_classic(base_size = 14)+
+eur_pc12 <- ggplot(eur_pca, aes(PC1, PC2, colour=Country,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC1", " ", "(", signif(eur_eig$var_perc[1], 3),"%", ")",  sep = ""), 
         y = paste("PC2", " ", "(", signif(eur_eig$var_perc[2], 3), "%",")",  sep = ""),
-        colour = "Country", shape = "Collection") + scale_color_manual(values = my_pal)
+        colour = "Country", shape = "Year of sampling") + scale_color_manual(values = my_pal)
 
 # PC1 vc PC3
-eur_pc13 <- ggplot(eur_pca, aes(PC1, PC3, colour=Country,shape=Collection)) + geom_point(size=3) + theme_classic(base_size = 14)+
+eur_pc13 <- ggplot(eur_pca, aes(PC1, PC3, colour=Country,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC1", " ", "(", signif(eur_eig$var_perc[1], 3),"%", ")",  sep = ""), 
         y = paste("PC3", " ", "(", signif(eur_eig$var_perc[3], 3), "%",")",  sep = ""),
-        colour = "Country", shape = "Collection") + scale_color_manual(values = my_pal) 
+        colour = "Country", shape = "Year of sampling") + scale_color_manual(values = my_pal) 
 
 #PC2 vs PC3
-eur_pc23 <- ggplot(eur_pca, aes(PC2, PC3, colour=Country,shape=Collection)) + geom_point(size=3) + theme_classic(base_size = 14)+
+eur_pc23 <- ggplot(eur_pca, aes(PC2, PC3, colour=Country,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC2", " ", "(", signif(eur_eig$var_perc[2], 3),"%", ")",  sep = ""), 
         y = paste("PC3", " ", "(", signif(eur_eig$var_perc[3], 3), "%",")",  sep = ""),
-        colour = "Country", shape = "Collection") + scale_color_manual(values = my_pal) 
+        colour = "Country", shape = "Year of sampling") + scale_color_manual(values = my_pal) 
 
-eur_all <- eur_pc12 + eur_pc13 + eur_pc23 + guide_area() + plot_layout(guides = "collect") & guides(shape=guide_legend(order = 1, ncol = 2 ), colour = guide_legend(order = 2))
+eur_all <- eur_pc12 + eur_pc13 + eur_pc23 + var_eur + plot_layout(guides = "collect")+plot_annotation(tag_levels = 'a')
 
 ggsave(eur_all, filename = "tritici_europe+_pca_plots.pdf", height = 30, width = 45, unit = "cm")
 
