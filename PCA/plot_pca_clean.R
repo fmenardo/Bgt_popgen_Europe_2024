40f7656bc552a91ad78aca774a9af34590dae386
Jigisha <90404355+jjigisha@users.noreply.github.com>
2025-01-24 13:43:29 +0100

    Update plot_pca_clean.R
    plot AUS in front of USA

--- PCA/plot_pca_clean.R
+++ PCA/plot_pca_clean.R
@@ -18,6 +18,8 @@ world_pca_reg$collection_3_levels <- ifelse(world_pca_reg$year_of_collection == 
                                             world_pca_reg$year_of_collection,
                                                     "1980-2019")
 
+world_pca_reg$global_region <- as.factor(world_pca_reg$global_region)
+
 # percentage of variance explained by each principal component
 var <- ggplot(data=world_eig[1:20,], aes(PC, var_perc))+geom_bar(stat = "identity")+
   theme_classic(base_size = 14)+
@@ -26,21 +28,33 @@ var <- ggplot(data=world_eig[1:20,], aes(PC, var_perc))+geom_bar(stat = "identit
   scale_x_continuous(breaks = c(1:20))
 
 # PC1 vs PC2
-world_pc12 <- ggplot(world_pca_reg, aes(PC1, PC2, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
+world_pc12 <- ggplot(world_pca_reg %>%
+                   arrange(factor(global_region, levels = c("Argentina","United States of America","China",
+                                                            "Egypt", "Israel", "Japan", "Northern Europe", "Russia/Central Asia",
+                                                            "Southern Europe", "Turkey", "Australia"))), 
+                     aes(PC1, PC2, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC1", " ", "(", signif(world_eig$var_perc[1], 3),"%", ")",  sep = ""), 
         y = paste("PC2", " ", "(", signif(world_eig$var_perc[2], 3), "%",")",  sep = ""),
         colour = "Region", shape = "Year of sampling") + scale_color_manual(values = as.vector(kelly(12))[2:12]) 
 
 # PC1 vs PC3
-world_pc13 <- ggplot(world_pca_reg, aes(PC1, PC3, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
+world_pc13 <- ggplot(world_pca_reg %>%
+                   arrange(factor(global_region, levels = c("Argentina","United States of America","China",
+                                                            "Egypt", "Israel", "Japan", "Northern Europe", "Russia/Central Asia",
+                                                            "Southern Europe", "Turkey", "Australia"))), 
+                     aes(PC1, PC3, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC1", " ", "(", signif(world_eig$var_perc[1], 3),"%", ")",  sep = ""), 
         y = paste("PC3", " ", "(", signif(world_eig$var_perc[3], 3), "%",")",  sep = ""),
         colour = "Region", shape = "Year of sampling") + scale_color_manual(values = as.vector(kelly(12))[2:12]) 
 
 #PC2 vs PC3
-world_pc23 <- ggplot(world_pca_reg, aes(PC2, PC3, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
+world_pc23 <- ggplot(world_pca_reg %>%
+                   arrange(factor(global_region, levels = c("Argentina","United States of America","China",
+                                                            "Egypt", "Israel", "Japan", "Northern Europe", "Russia/Central Asia",
+                                                            "Southern Europe", "Turkey", "Australia"))), 
+                     aes(PC2, PC3, colour=global_region,shape=collection_3_levels)) + geom_point(size=3) + theme_classic(base_size = 14)+
   theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))+
   labs (x = paste("PC2", " ", "(", signif(world_eig$var_perc[2], 3),"%", ")",  sep = ""), 
         y = paste("PC3", " ", "(", signif(world_eig$var_perc[3], 3), "%",")",  sep = ""),
