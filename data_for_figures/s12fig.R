daf2ecc558e43ed6beb12b57cce76b8b9677be33
Jigisha <90404355+jjigisha@users.noreply.github.com>
2025-02-28 13:58:01 +0100

    Update s12fig.R

--- data_for_figures/s12fig.R
+++ data_for_figures/s12fig.R
@@ -4,3 +4,26 @@
 # (b) -> ../MMC/sfs_data_E_EUR2.csv
 # (c) -> ../MMC/sfs_data_N_EUR1.csv
 # (d) -> ../MMC/sfs_data_E_EUR1.csv
+library(ggplot2)
+
+types <- c("Observed"="black","Beta"="red","KM"="blue")
+
+# eg. E_EUR1
+sfs <- read.csv("../MMC/sfs_data_E_EUR1.csv")
+plot1 <-  ggplot(data=sfs) + geom_point(aes(y=observed,
+                                    x=seq(along = observed),
+                                          colour="Observed")) +
+    geom_line(aes(y=beta,x=seq(along = observed),colour = "Beta")) +
+    geom_line(aes(y=km,x=seq(along = observed),colour="KM")) +
+    labs(x = "SFS class", y= "Fraction") +
+    labs(title=paste0("SFS ",short_name)) +
+    scale_colour_manual(values=types) +
+    theme_classic() +
+    theme(legend.text = element_text(size = 11),
+          legend.background = element_rect(linewidth = .1,
+                                           colour = "black" 
+                                            ),
+          legend.title=element_blank(),
+          legend.position="inside",
+          legend.justification = c(1,1))
+
