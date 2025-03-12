7662514e5df9334d696973ac3c2f2760bc08a48a
Jigisha <90404355+jjigisha@users.noreply.github.com>
2025-03-12 10:24:58 +0100

    Update and rename s9fig.R to FigI_S1.R

--- data_for_figures/FigI_S1.R
+++ data_for_figures/FigI_S1.R
@@ -1,4 +1,4 @@
-#### Fig 9 ####
+#### Fig I in S1 Text ####
 # LD decay
 # plots -> ../Linkage_Disequilibrium/LD_decay_plink_10kb_onePlot_legend_fixed.R
 # For more details, see ../Linkage_Disequilibrium/LD.md
